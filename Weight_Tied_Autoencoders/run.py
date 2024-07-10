92249f25dd07620eea786783a1880eab3bbbfe99
khachaturyan-1991 <dr.khachaturyan@gmail.com>
2024-07-10 20:40:19 +0200

    separated Wieght Tiyng to its own folder

--- Weight_Tied_Autoencoders/run.py
+++ Weight_Tied_Autoencoders/run.py
@@ -2,68 +2,84 @@ import tensorflow as tf
 import matplotlib.pylab as plt
 import time
 
+from args_parse import parse_arguments
 from autoencoder import Autoencoder, AutoencoderZeroDecoder
 from utils import train_fn, dice_loss
 from data import create_dataloader
 
-device = "/GPU:0" if tf.config.list_physical_devices('GPU') else "/cpu:0"
-print("Device: ", device)
 
-BATCH_SIZE = 32
-IMG_SIZE = 128
+if __name__ == "__main__":
 
-train_dataloader = create_dataloader(img_dir="../../data/clothing/images",
-                                     target_size=IMG_SIZE, batch_size=BATCH_SIZE,
-                                     first_img=0, last_img=32)
-test_dataloader = create_dataloader(img_dir="../../data/clothing/images",
-                                    target_size=IMG_SIZE, batch_size=BATCH_SIZE,
-                                    first_img=900, last_img=950)
-val_dataloader = create_dataloader(img_dir="../../data/clothing/images",
-                                   target_size=IMG_SIZE, batch_size=BATCH_SIZE,
-                                   first_img=0, last_img=32)
+    args = parse_arguments()
+    BATCH_SIZE = args.batch_size
+    IMG_SIZE = args.img_size
+    DEVICE = f"/{args.device.lower()}:0"
+    EPOCHS = args.epochs
+    OUTPUT_FREQUENCY = args.output_freq
+    SAVE_HISTORY = args.save_history
+    MODEL_TYPE = args.model.lower()
+    DATA_FOLDER = args.data_folder
 
-model = Autoencoder()
-input_tensor = tf.random.normal([1, 128, 128, 1])
-output_tensor = model(input_tensor)
-print("Input tensor shape: ", input_tensor.shape)
-print("Output tensor shape: ", output_tensor.shape)
-print(model.summary())
+    models_list = ["autoencoder", "zero_decoder"]
 
-optim = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    train_dataloader = create_dataloader(img_dir=f"{DATA_FOLDER}",
+                                         target_size=IMG_SIZE, batch_size=BATCH_SIZE,
+                                         first_img=0, last_img=32)
+    test_dataloader = create_dataloader(img_dir=f"{DATA_FOLDER}",
+                                        target_size=IMG_SIZE, batch_size=BATCH_SIZE,
+                                        first_img=900, last_img=950)
+    val_dataloader = create_dataloader(img_dir=f"{DATA_FOLDER}",
+                                       target_size=IMG_SIZE, batch_size=BATCH_SIZE,
+                                       first_img=0, last_img=32)
 
-t1 = time.time()
+    assert MODEL_TYPE in models_list, "Model type mast be either autoencoder or zero_decoder"
+    if MODEL_TYPE == "zero_decoder":
+        model = AutoencoderZeroDecoder()
+    else:
+        model = Autoencoder()
 
-FIRST_STEP, EPOCHS = 0, 30
-h_train, h_test = train_fn(model=model,
-                           train_dataloader=train_dataloader,
-                           val_dataloader=val_dataloader,
-                           loss_fn=dice_loss,
-                           optimizer=optim,
-                           first_step=FIRST_STEP,
-                           last_step=FIRST_STEP + EPOCHS,
-                           device="\CPU",
-                           output_freq=5)
+    input_tensor = tf.random.normal([1, IMG_SIZE, IMG_SIZE, 1])
+    output_tensor = model(input_tensor)
+    print("Input tensor shape: ", input_tensor.shape)
+    print("Output tensor shape: ", output_tensor.shape)
+    print(model.summary())
 
-model.save('autoencoder_zero_decoder_cloth.keras')
+    optim = tf.keras.optimizers.Adam(learning_rate=1e-4)
 
-t2 = time.time()
-print("Time Spent: ", (t2 - t1) // 60)
+    t1 = time.time()
 
+    FIRST_STEP = 0
+    h_train, h_test = train_fn(model=model,
+                               train_dataloader=train_dataloader,
+                               val_dataloader=val_dataloader,
+                               loss_fn=dice_loss,
+                               optimizer=optim,
+                               first_step=FIRST_STEP,
+                               last_step=FIRST_STEP + EPOCHS,
+                               device=DEVICE,
+                               output_freq=OUTPUT_FREQUENCY)
 
-fig = plt.figure(figsize=(6, 6))
+    model.save(f'{MODEL_TYPE}.keras')
 
-plt.plot(h_train.keys(),
-         h_train.values(),
-         "-o", label="Train")
+    t2 = time.time()
+    print("Time Spent: ", (t2 - t1) // 60)
 
-plt.plot(h_test.keys(),
-         h_test.values(),
-         "-*", label="Validation")
+    if SAVE_HISTORY:
 
-plt.legend(title="Dataset")
+        fig = plt.figure(figsize=(6, 6))
 
-plt.grid(True)
-plt.xlabel("epoch")
-plt.ylabel("Loss")
-plt.ylim(0, 1)
-plt.savefig("loss_history_auto.png")
+        plt.plot(h_train.keys(),
+                 h_train.values(),
+                 "-o", label="Train")
+
+        plt.plot(h_test.keys(),
+                 h_test.values(),
+                 "-*", label="Validation")
+
+        plt.legend(title="Dataset")
+
+        plt.grid(True)
+        plt.xlabel("epoch")
+        plt.ylabel("Loss")
+        plt.ylim(0, 1)
+        plt.savefig(f"{SAVE_HISTORY}.png")
